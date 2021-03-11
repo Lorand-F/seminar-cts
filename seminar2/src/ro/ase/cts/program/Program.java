ac6e1480fd6f3b5fcda447ebaab3019bd8752ab0
Lorand <formagiu.lorand@gmail.com>
2021-03-11 12:16:19 +0200

    seminary 3 - commit A (changes on read mode)

--- seminar2/src/ro/ase/cts/program/Program.java
+++ seminar2/src/ro/ase/cts/program/Program.java
@@ -8,13 +8,19 @@ import ro.ase.cts.clase.Aplicant;
 import ro.ase.cts.clase.UniversalReader;
 import ro.ase.cts.clase.readers.EmployeeReader;
 import ro.ase.cts.clase.readers.PupilReader;
+import ro.ase.cts.clase.readers.Reader;
+import ro.ase.cts.clase.readers.StudentReader;
 
 public class Program {
+	
+	public static List<Aplicant> readAplicants(Reader reader) throws FileNotFoundException{
+		return reader.readAplicants();
+	}
 
 	public static void main(String[] args) {
 		List<Aplicant> listaAplicanti;
 		try {
-			listaAplicanti = UniversalReader.readAplicants(new PupilReader("elevi.txt"));	
+			listaAplicanti = UniversalReader.readAplicants(new StudentReader("studenti.txt"));	
 			for(Aplicant aplicant:listaAplicanti)
 				System.out.println(aplicant.toString());
 		} catch (FileNotFoundException e) {
